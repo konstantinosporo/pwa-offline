6f4ef10c4326b23f1f21606089b9210eabd6c658
konstantinosporo <konstantinosporo@hotmail.gr>
2025-09-27 18:42:51 +0300

    remove whitespaces

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,6 @@ import {
   isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideServiceWorker } from '@angular/service-worker';
