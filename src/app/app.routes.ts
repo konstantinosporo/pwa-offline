0a5a592b9f074a096e2c18bb7f3875a09f3c9c47
konstantinosporo <konstantinosporo@hotmail.gr>
2025-09-28 01:14:55 +0300

    dynamic nav routing/ active / small mods on welcome card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Product } from './features/product/product';
-import { Test } from './core/test/test/test';
 import { Layout } from './features/layout/layout';
 import { Home } from './features/home/home';
 
