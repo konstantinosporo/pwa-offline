eb881d211c8bc3f4d13ba130920fc37059288564
konstantinosporo <konstantinosporo@hotmail.gr>
2025-09-27 21:28:00 +0300

    testing new version message

--- src/app/core/services/sw/update/update.ts
+++ src/app/core/services/sw/update/update.ts
@@ -10,7 +10,19 @@ export class Update {
   updateApp() {
     this.sw.versionUpdates.subscribe((update) => {
       if (update.type === 'VERSION_READY') {
-        if (confirm('New version available. Load new version?')) {
+        const current = update.currentVersion.hash.substring(0, 7);
+        const latest = update.latestVersion.hash.substring(0, 7);
+
+        const message = `
+A new version of this app is available.
+
+Current version: ${current}
+New version:     ${latest}
+
+Would you like to load the new version now?
+      `;
+
+        if (confirm(message)) {
           window.location.reload();
         }
       }
